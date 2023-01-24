fb1bb64447b133c0654b7ac644fe6b5fc9bb4339
Thomas's Laptop <vo.thomas.11647@outlook.com>
2023-01-24 20:18:11 +1100

    rename dir

---
+++ MainCodingTracker/Program.cs
@@ -0,0 +1,180 @@
+﻿namespace CodingTracker;
+
+class Program
+{
+    static DatabaseCreation database = new();
+    static void Main()
+    {
+        MainMenu();
+    }
+    static void MainMenu()
+    {
+       
+        CRUDController action = new();
+        UserInput input = new();
+        bool endApp = false;
+
+        while (!endApp)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("\n-------------------------------\n");
+            Console.WriteLine("\tCODING TRACKER");
+            Console.WriteLine("\n-------------------------------\n");
+            Console.WriteLine($"Welcome, {database.Name.Replace("_", " ")}!. Today {(DateTime.Now).ToString("dd - MM - yyyy")}");
+            Console.WriteLine("\n0*. Start Tracking. . .");
+            Console.WriteLine("-------------------------------");            
+            Console.WriteLine("1. View Records.");
+            Console.WriteLine("2. Insert Records.");
+            Console.WriteLine("3. Delete Records.");
+            Console.WriteLine("4. Update Records.");
+            Console.WriteLine("5*. See Record Report.");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("6. CREATE YOUR OWN TRACKER !");            
+            Console.WriteLine("-------------------------------\n");
+            Console.WriteLine("7. Exit.\n");
+            Console.Write("Your option: ");
+
+            var userOption = Console.ReadLine();
+            Console.Clear();
+
+            switch (userOption)
+            {
+                case "0":
+                    string date = DateTime.Now.ToString("dd-MM-yy");
+                    string startTime = DateTime.Now.ToString("HH:mm");
+                    string endTime = DateTime.Now.ToString("HH:mm");
+
+                    Console.WriteLine($"Timer start now.\nToday: {DateTime.Now.ToString("dd - MM - yyyy")}.");
+                    Console.WriteLine("-------------------------------\n");
+                    Console.WriteLine($"Start Time: {startTime}");
+                    Console.Write("\nPress SPACEBAR to stop the Timer");
+                    if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                    {
+                        Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+                        Console.WriteLine($"Stop Time: {endTime}");
+                        Console.WriteLine("-------------------------------");
+                        List<int> timerList = input.GetConcurrentTimeList(startTime, endTime);
+                        action.Insert(database.Name, date, timerList);
+                        Console.WriteLine("Timer added to the record.");
+                    }
+                    TaskComplete();
+                    Console.ReadLine();                    
+                    break;
+                case "1":
+                    action.GetAllRecords(database.Name);
+                    TaskComplete();
+                    break;
+                case "2":
+                    input.GetRecordFromUser(database.Name);
+                    TaskComplete();
+                    break;
+                case "3":
+                    action.Delete(database.Name);
+                    TaskComplete();
+                    break;
+                case "4":
+                    action.Update(database.Name);
+                    TaskComplete();
+                    break;
+                case "5":
+                    ReportTable(database.Name);
+                    TaskComplete();
+                    break;
+                case "6":
+                    database.Name = database.CreateNewRecord();                    
+                    TaskComplete();
+                    break;
+                case "7":
+                    Console.WriteLine("\nGoodbye!\n");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Write("\nInvalid input! Please try again.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+    internal static void TaskComplete()
+    {
+        Thread.Sleep(500);
+        Console.WriteLine("-------------------------------");
+        Console.Write("Task completed. Press ENTER to continue.");
+        Console.ReadLine();
+        MainMenu();
+    }
+    static void ReportTable(string table)
+    {
+        CRUDController action = new();
+
+        bool reportable = action.Report(table);
+        bool endReport = false;
+
+        while (reportable && !endReport)
+        {
+            action.GetAllRecords(table);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"\tREPORT OPTION\r");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1. Filter Record");
+            Console.WriteLine("2. Report of the Record");
+            Console.WriteLine("2. Set your own goals");
+            Console.WriteLine("3. Return to Menu");
+            Console.WriteLine("-------------------------------");
+            Console.Write("Your option: ");
+            var input = Console.ReadLine();
+
+            switch(input)
+            {
+                case "1":
+                    ReportTableOption1(table);
+                    break;
+                case "2":
+                    Console.WriteLine("");
+                    break;
+                case "3":
+                    endReport = true;
+                    break;
+                default:
+                    Console.Write("\nInvalid input! Please try again.");
+                    Console.ReadLine();
+                    ReportTable(table);
+                    break;
+            }
+
+            return;
+        }        
+    }
+    static void ReportTableOption1(string table)
+    {
+        CRUDController action = new();
+
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Period of time");
+        Console.WriteLine("------------------");
+        Console.WriteLine("1. This Month");
+        Console.WriteLine("2. This Year");
+        Console.WriteLine("3. Go back to Report Menu");
+        Console.WriteLine("-------------------------------");
+        Console.Write("Your option: ");
+        var input = Console.ReadLine();       
+
+        switch(input)
+        {
+            
+            case "1":                
+                action.ReportMontly(table);
+                Console.ReadLine();
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case"4":
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ MainCodingTracker/UserInput.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    public class UserInput
+    {
+                
+        Validation val = new();
+        public void GetRecordFromUser(string table)
+        {
+            CRUDController action = new();
+            string dateInsert = GetDate();
+            List<int> timeInsert = GetUserTime();
+            action.Insert(table,dateInsert, timeInsert);
+        }
+        public List<int> GetConcurrentTimeList(string start, string end)
+        {            
+            var startT = start.Split(":").Select(Int32.Parse).ToList();
+            var endT = end.Split(":").Select(Int32.Parse).ToList();
+            List<int> timeList = startT.Concat(endT).ToList();
+            timeList.Add(val.CalculateDuration(timeList));
+
+            return timeList;
+        }
+        public string GetDate()
+        {
+            Console.Write("Insert the date (Format dd-MM-yy): ");
+            string dateInput = Console.ReadLine();
+
+            while(!val.CheckDateInput(dateInput))
+            {
+                Console.Write("\n\nInvalid date. (Formate: dd-MM-yy). Try again: ");
+                dateInput = Console.ReadLine();
+            }           
+
+            return dateInput;
+        }
+        internal int GetHour(string input)
+        {
+            Console.Write(input);
+
+            string numberInput = Console.ReadLine();
+
+            while (!val.CheckHourInput(numberInput))
+            {
+                Console.Write("Invalid number. Hour has to be between (0-23). Try again: ");
+                numberInput = Console.ReadLine();
+
+            }
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
+        }
+        internal int GetMin(string input)
+        {
+            Console.Write(input);
+
+            string numberInput = Console.ReadLine();
+
+            while (!val.CheckMinInput(numberInput))
+            {
+                Console.Write("Invalid number. Minute has to be between (0-60). Try again: ");
+                numberInput = Console.ReadLine();
+
+            }
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
+        }
+        public List<int> GetUserTime()
+        {
+            // Set a list in which endTime Hour is less than
+            // StartTime then loop if endT still < StartT.
+            int[] defaultTime = { 1, 0, 0, 0 };
+            List<int> timeInsert = new(defaultTime);
+
+            //Not sure this is the optimal way to do this?
+            while (timeInsert[2] < timeInsert[0])
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Time format - 24 hour");
+                timeInsert.Clear();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Start Time");
+                    }
+                    else
+                    {
+                        Console.WriteLine("---------------");
+                        Console.WriteLine("End Time");
+                    }
+                    int timeInputHour = GetHour("Hour: ");
+                    int timeInputMinute = GetMin("Minute: ");
+                    timeInsert.Add(timeInputHour);
+                    timeInsert.Add(timeInputMinute);
+                }
+                if (timeInsert[2] < timeInsert[0])
+                {
+                    Console.WriteLine("EndTime cannot be before StartTime. Press ENTER to try again.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                if (timeInsert[0] == timeInsert[2] && timeInsert[1] == timeInsert[3])
+                {
+                    Console.WriteLine("EndTime and StartTime are the same. Press ENTER to try again.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            timeInsert.Add(val.CalculateDuration(timeInsert));
+            return timeInsert;
+        }        
+    }
+}
